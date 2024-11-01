7dce05155d9d37150e77acc4226af22a04e37da2
teddy.nguyen <teddy.nguyen@techvify.com.vn>
2024-11-01 16:35:19 +0700

    update service

--- lib/src/schemas/users/auth-user.schema.ts
+++ lib/src/schemas/users/auth-user.schema.ts
@@ -1,9 +1,24 @@
-import { IsString } from 'class-validator';
+import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
 
 export class AuthUser{
   @IsString()
+  @IsNotEmpty({message: 'không được để trống !'})
+  @MinLength(3, {
+    message: "Tên tài khoản quá ngắn"
+  })
+  @MaxLength(20, {
+      message: "Tên tài khoản quá dài"
+  })
   username!: string;
 
   @IsString()
+  @IsNotEmpty({message: 'không được để trống !'})
+  @MinLength(3, {
+    message: "Tên tài khoản quá ngắn"
+  })
+  @MaxLength(20, {
+      message: "Tên tài khoản quá dài"
+  })
   password!: string;
 }
+ 

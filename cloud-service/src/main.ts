3795335279f415aaf27ff1a45e268f3a1b660fd5
mint_duc <mintduc2612@gmail.com>
2025-02-27 23:00:47 +0700

    CORS for aws upload

--- cloud-service/src/main.ts
+++ cloud-service/src/main.ts
@@ -4,7 +4,15 @@ import { env } from 'src/configs/env.config';
 import { AwsModule } from './application/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AwsModule);
+  // const app = await NestFactory.create(AwsModule);
+  const app = await NestFactory.create(AwsModule,
+    {
+      cors: {
+        origin: 'http://localhost:8080',
+        credentials: true,
+      },
+    }
+  );
   app.connectMicroservice<MicroserviceOptions>({
     transport: Transport.TCP,
     options: {
@@ -15,5 +23,22 @@ async function bootstrap() {
   // await app.startAllMicroservices();
   await app.listen(env.APP.AWS_SERVICE.PORT);
   console.log('listen port ' + env.APP.AWS_SERVICE.PORT + '...');
+
+  const gracefulShutdown = (): void => {
+    console.log('Shutting down gracefully...')
+    
+    app.close()
+    
+    // Force close the server after 5 seconds
+    setTimeout(() => {
+      console.error(
+        'Could not close connections in time, forcefully shutting down',
+      )
+        process.exit(1)
+      }, 5000)
+  }
+    
+  process.on('SIGTERM', gracefulShutdown)
+  process.on('SIGINT', gracefulShutdown)
 }
 bootstrap();
